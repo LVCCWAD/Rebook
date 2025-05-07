4d12af54be8731a963b3a7d1aaf57c2d793b3e23
Jon <aljonkenfernandez36@gmail.com>
2025-05-07 10:12:18 +0800

    Cart page, Product Page, Seller page,

---
+++ resources/js/Components/SellerComponents/Body.jsx
@@ -0,0 +1,15 @@
+import React  from "react";
+import Dashboard from "./Body/Dashboard";
+
+// retrieve list of seller body component
+
+export default function Body(){
+    return(
+        <>
+            {/* --- BODY --- */}
+
+                {/* --- DASHBOARD --- */}
+                <Dashboard />
+        </>
+    )
+}
