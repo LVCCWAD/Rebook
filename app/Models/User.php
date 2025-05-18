1ee36642911fdfa8414b3d9f6250c01ceca1e000
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-05-18 23:31:42 +0800

    some changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,11 @@ class User extends Authenticatable
         return $this->hasOne(Shop::class, 'seller_id');
     }
 
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
     public function orders(): HasMany
     {
         return $this->hasMany(Order::class);
